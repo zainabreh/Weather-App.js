2686d4104d394a10ad666347154049b550b26713
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-07-10 10:59:31 +0500

    Data fetching complete

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+let srchinput = document.getElementsByClassName('srchinput')[0]
+let srchbtn = document.getElementsByClassName("srchbtn")[0]
+const API_KEY = "44049957cd39c54716054a34be36db63";
+let newcountry
+srchbtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    newcountry = srchinput.value
+    fetchData(newcountry)
+})
+const fetchData = (newcountry = 'islamabad')=>{
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newcountry}&units=metric&appid=${API_KEY}`).then((res)=>res.json()).then((data)=>{
+        console.log(data);
+    })
+} 
+
+fetchData()
